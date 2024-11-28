9de89ad290e0dabdb9bb4c7d7a3e72c16a59331e
lcampion <50773125+Niikoneko@users.noreply.github.com>
2024-11-28 22:19:15 +0100

    Modification de la méthode de gestion du support

--- src/main/java/org/niikoneko/encrier/userInterface/formulaires/SignalerBugController.java
+++ src/main/java/org/niikoneko/encrier/userInterface/formulaires/SignalerBugController.java
@@ -1,18 +1,14 @@
 package org.niikoneko.encrier.userInterface.formulaires;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.*;
-import javafx.scene.paint.Color;
+import javafx.scene.control.Button;
 import javafx.stage.Stage;
-import org.niikoneko.encrier.Main;
 import org.tinylog.Logger;
 
+import java.awt.*;
 import java.io.IOException;
-import java.net.URI;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
-import java.time.Duration;
+import java.net.URISyntaxException;
+import java.net.URL;
 
 /**
  * Controller pour le formulaire de signalement de bugs
@@ -23,112 +19,33 @@ import java.time.Duration;
 public class SignalerBugController {
 
     @FXML
-    private Label errorMessage;
-    @FXML
-    private TextField titre;
-    @FXML
-    private TextArea description;
-    @FXML
-    private Button annuler;
-    @FXML
-    private Button envoi;
-    @FXML
-    private Label progressMessage;
-    @FXML
-    private ProgressBar sendProgress;
-
-    private boolean flagSent = false;
+    private Button closeButton;
 
     @FXML
-    protected void onAnnulerClick() {
-        Stage current = (Stage) annuler.getScene().getWindow();
+    protected void onFermerClick() {
+        Stage current = (Stage) closeButton.getScene().getWindow();
         current.close();
     }
 
     @FXML
-    protected void onEnvoyerClick() {
-        if (flagSent) {
-            onAnnulerClick();
-            return;
-        }
-        sendProgress.setProgress(0.25);
-        progressMessage.setText("Vérification");
-        if (!checkData()) return;
-        sendProgress.setProgress(0.5);
-        progressMessage.setText("Préparation");
-        Logger.info("Préparation d'envoi d'un bug");
-        String address = "https://api.github.com/repos/" + Main.getProperty("github_owner")
-                + "/" + Main.getProperty("github_repo") + "/issues";
-        String data = buildData();
-        HttpClient client = HttpClient.newBuilder()
-                .version(HttpClient.Version.HTTP_2)
-                .followRedirects(HttpClient.Redirect.NORMAL)
-                .connectTimeout(Duration.ofSeconds(30))
-                .build();
-        HttpRequest issueReq = HttpRequest.newBuilder()
-                .uri(URI.create(address))
-                .timeout(Duration.ofMinutes(2))
-                .header("Accept", "application/vnd.github+json")
-                .header("Authorization", "Bearer " + Main.getProperty("github_token"))
-                .header("X-GitHub-Api-Version", "2022-11-28")
-                .POST(HttpRequest.BodyPublishers.ofString(data))
-                .build();
-        sendProgress.setProgress(0.75);
-        progressMessage.setText("Envoi");
+    protected void onSupportFormClick() {
         try {
-            HttpResponse<String> response = client.send(issueReq, HttpResponse.BodyHandlers.ofString());
-            if (response.statusCode() != 201) {
-                Logger.error("Réponse reçue en erreur : code HTTP {}, corps de réponse {}", response.statusCode(),
-                        response.body());
-                Logger.error("Corps de requête envoyé : {}", data);
-                errorMessage.setText("Erreur lors de l'envoi, code de réponse : " + response.statusCode() +
-                        ". Rendez-vous sur la page du projet pour signaler votre problème.");
-            } else {
-                sendProgress.setProgress(1.0);
-                progressMessage.setText("Envoi reçu !");
-                progressMessage.setTextFill(Color.GREEN);
-                Logger.info("Bug envoyé");
-                flagSent = true;
-                envoi.setText("Fermer");
-            }
+            Desktop.getDesktop().browse(new URL("https://forms.gle/6roaUfudc5DDR1JV6").toURI());
         } catch (IOException e) {
-            Logger.error("Erreur I/O à l'envoi de la requête", e);
-            errorMessage.setText("Erreur lors de l'envoi : souci I/O. Veuillez réessayer.");
-        } catch (InterruptedException e) {
-            Logger.error("Interruption à l'envoi de la requête", e);
-            errorMessage.setText("Erreur lors de l'envoi : Interruption du processus. Veuillez réessayer.");
+            Logger.error("Erreur IO à l'ouverture du lien :", e);
+        } catch (URISyntaxException e) {
+            Logger.error("Erreur de syntaxe URL :", e);
         }
     }
 
-    /**
-     * Permet de vérifier les paramètres du formulaire
-     * @return true si tout est OK, paramètre les erreurs sinon
-     */
-    private boolean checkData() {
-        if (titre.getText().isEmpty() || titre.getText().length() < 20) {
-            errorMessage.setText("Vous devez indiquer un titre d'au moins 20 caractères");
-            sendProgress.setProgress(0);
-            progressMessage.setText("");
-            return false;
+    @FXML
+    protected void onDiscordClick() {
+        try {
+            Desktop.getDesktop().browse(new URL("https://discord.gg/DyQN5NYEXh").toURI());
+        } catch (IOException e) {
+            Logger.error("Erreur IO à l'ouverture du lien :", e);
+        } catch (URISyntaxException e) {
+            Logger.error("Erreur de syntaxe URL :", e);
         }
-        if (description.getText().isEmpty() || description.getText().length() < 50) {
-            errorMessage.setText("Vous devez indiquer une description d'au moins 50 caractères");
-            sendProgress.setProgress(0);
-            progressMessage.setText("");
-            return false;
-        }
-        return true;
-    }
-
-    /**
-     * Construit le bloc data de la requête
-     * @return le bloc data construit
-     */
-    private String buildData() {
-        return "{\"title\":\"" +
-                titre.getText() +
-                "\",\"body\":\"" +
-                description.getText() +
-                "\",\"assignees\":[\"Niikoneko\"],\"labels\":[\"bug\"]}";
     }
 }
